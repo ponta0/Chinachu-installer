3fe582814bef849deb4f8a9e427f69ce9ab361aa
まぐにあ <hoge@example.com>
2021-01-23 16:07:03 +0900

    edit px-w3u4.sh

--- tuners/px-w3u4/px-w3u4.sh
+++ tuners/px-w3u4/px-w3u4.sh
@@ -14,6 +14,8 @@ fi
 
 if [ $1 = "install" ]; then
     echo "インストールを開始します"
+    # 依存パッケージのインストール
+    sudo apt install -y git build-essential dkms
     # ファームウェアの抽出とインストール
     git clone https://github.com/nns779/px4_drv.git
     cd px4_drv
