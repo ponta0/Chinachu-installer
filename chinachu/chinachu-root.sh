a8c16878b5ebea575b82f4df95d11da7ea239eff
まぐにあ <hoge@example.com>
2021-01-23 21:37:13 +0900

    edit scripts

--- chinachu/chinachu-root.sh
+++ chinachu/chinachu-root.sh
@@ -1,4 +1,28 @@
 #! /bin/bash
 
-pm2 start processes.json
-pm2 save
+
+if [ "$1" = "" ] ; then
+    echo "オプションを指定してください(help,install,uninstall)"
+    exit 1
+fi
+
+if [ $1 = "help" ] ; then
+    echo "使い方"
+    echo "インストールの場合は"install"オプションを使ってください"
+    echo "アンインストールの場合は"uninstall"オプションを使ってください"
+    exit 1
+fi
+
+if [ $1 = "install" ] ; then
+    echo "インストールを開始します"
+    pm2 start processes.json
+    pm2 save
+    exit 1
+fi
+
+if [ $1 = "uninstall" ] ; then
+    #echo "アンインストールを開始します"
+    echo "この機能は未実装です"
+    #echo "アンインストールが完了しました"
+    exit 1
+fi
